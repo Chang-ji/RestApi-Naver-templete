5200b87adf3632128f62aa6e27dc3f64be2da244
Chang-ji <75286090+Chang-ji@users.noreply.github.com>
2021-08-27 17:33:12 +0900

    first commit

---
+++ src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
@@ -0,0 +1,9 @@
+package com.example.restaurant.wishlist.repository;
+
+import com.example.restaurant.db.MemeryDbRepositoryAbstract;
+import com.example.restaurant.wishlist.Entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemeryDbRepositoryAbstract<WishListEntity> {
+}
